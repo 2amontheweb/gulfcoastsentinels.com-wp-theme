b1b992952b5da702504be780ccb2cf84bd5650f5
Jimmy Pocock <jimmypocock@yahoo.com>
2015-12-19 12:19:32 -0600

    Updated all the code to my liking. Everything is working.

--- footer.php
+++ footer.php
@@ -11,18 +11,18 @@
 
 ?>
 
-	</div><!-- #content -->
+      <footer id="footer-main" class="site-footer" role="contentinfo">
+        <div class="site-info">
+          <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'default-bootstrap-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'default-bootstrap-theme' ), 'WordPress' ); ?></a>
+          <span class="sep"> | </span>
+          <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'default-bootstrap-theme' ), 'default-bootstrap-theme', '<a href="http://jimmypocock.com" rel="designer">Jimmy Pocock</a>' ); ?>
+        </div><!-- .site-info -->
+      </footer><!-- #colophon -->
+    </div><!-- #page -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'default-bootstrap-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'default-bootstrap-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'default-bootstrap-theme' ), 'default-bootstrap-theme', '<a href="http://jimmypocock.com" rel="designer">Jimmy Pocock</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+    <?php wp_footer(); ?>
 
-<?php wp_footer(); ?>
+    <script src="<?php bloginfo('template_directory') ?>/js/bundled.js"></script>
 
-</body>
+  </body>
 </html>
--- header.php
+++ header.php
@@ -11,40 +11,64 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <!-- <link rel="shortcut icon" href="<?#php echo get_stylesheet_directory_uri(); ?>/favicon.ico" /> -->
 
-<?php wp_head(); ?>
-</head>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/css/bootstrap.min.css" rel="stylesheet">
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'default-bootstrap-theme' ); ?></a>
+    <!-- Font Awesome Icons -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/css/font-awesome.min.css" rel="stylesheet">
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+    <!-- Bootstrap core JavaScript & jQuery
+    ================================================== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_directory') ?>/js/bootstrap.min.js"></script>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+    <?php wp_head(); ?>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'default-bootstrap-theme' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    <!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-	<div id="content" class="site-content">
+  <body <?php body_class(); ?>>
+    <div id="page" class="site">
+      <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'default-bootstrap-theme' ); ?></a>
+
+      <header id="header-main" class="site-header" role="banner">
+        <nav class="navbar">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="/">
+                <h1>Default Bootstrap Theme</h1>
+              </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <?php
+              wp_nav_menu( array(
+
+                'theme_location' => 'header',
+                'container'       => 'nav',
+                'container_class' => 'navbar-collapse collapse',
+                'menu_class'        => 'nav navbar-nav navbar-right'
+
+              ) );
+            ?>
+          </div><!-- /.container -->
+        </nav>
+      </header><!-- #masthead -->
