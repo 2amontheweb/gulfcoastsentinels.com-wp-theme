6b246618ddb8440a05d29c4b533403002193bd56
Jimmy Pocock <jimmypocock@yahoo.com>
2015-12-20 14:49:24 -0600

    Created and styled footer

--- footer.php
+++ footer.php
@@ -11,18 +11,34 @@
 
 ?>
 
-      <footer id="footer-main" class="site-footer" role="contentinfo">
+      <footer id="footer-main" role="contentinfo">
         <div class="container">
-          <div class="site-info">
-            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'default-bootstrap-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'default-bootstrap-theme' ), 'WordPress' ); ?></a>
-            <span class="sep"> | </span>
-            <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'default-bootstrap-theme' ), 'default-bootstrap-theme', '<a href="http://jimmypocock.com" rel="designer">Jimmy Pocock</a>' ); ?>
-          </div><!-- .site-info -->
+          <div class="row">
+            <div class="col-sm-8">
+              <h4>Get Involved</h4>
+              <h6>Interested in getting involved?</h6>
+              <p>If you would like to volunteer for the Gulf Coast Sentinels Scholarship Foundation, we would love to talk to you. Email us at <a href="mailto:lorena@sentinelsoffreedomgulfcoast.org">Lorena@sentinelsoffreedomgulfcoast.org</a>.</p>
+              <p>Website created by <a href="http://jimmypocock.com">Jimmy Pocock</a> with <a href="https://wordpress.org">WordPress</a></p>
+            </div>
+            <div class="col-sm-4">
+              <h4>Connect With Us</h4>
+              <ul>
+                <li><a href="https://www.facebook.com/Gulf-Coast-Sentinels-349914720144/" target="_blank"><i class="fa fa-facebook"></i>Facebook</a></li>
+                <li><a href="https://www.youtube.com/channel/UCvI6rqUomw6lhF4hEKp2EaQ" target="_blank"><i class="fa fa-youtube"></i>YouTube</a></li>
+                <li><a href="mailto:lorena@sentinelsoffreedomgulfcoast.org"><i class="fa fa-envelope"></i>Email</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="copyright">
+            <p>© <?php echo date("Y"); ?> Gulf Coast Sentinels</p>
+          </div>
         </div>
-      </footer><!-- #colophon -->
+      </footer><!-- #footer-main -->
+
+      <?php wp_footer(); ?>
+
     </div><!-- #page -->
 
-    <?php wp_footer(); ?>
 
     <script src="<?php bloginfo('template_directory') ?>/js/bundled.js"></script>
 
